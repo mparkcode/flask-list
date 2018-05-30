18eb7610e1c3340ddfb93c852ce6a3bc7d41e7c5
Michael Park <mparkcode@gmail.com>
2018-05-30 15:18:24 +0000

    css added

---
+++ flasklist.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, redirect, render_template, request
+from random import choice
+
+app=Flask(__name__)
+
+welcome_message = ["Hi", "Hello", "Bonjour", "Ciao", "Hola"]
+
+items = [
+    "Buy computer",
+    "Learn Python"
+    ]
+
+@app.route("/")
+def get_index():
+    welcome = choice(welcome_message)
+    return render_template("index.html", items=items, welcome=welcome)
+    
+@app.route("/new_task", methods=["POST"])
+def create_task():
+    task = request.form['task_to_do']
+    items.append(task)
+    return redirect("/")
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
